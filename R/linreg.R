686e849c66ed6e5e74970616fee7cbae396f52eb
Simon Alsen <simal451@student.liu.se>
2021-09-15 16:37:55 +0200

    Commit 1

---
+++ R/linreg.R
@@ -0,0 +1,52 @@
+linreg <- setRefClass("linreg",
+                      fields = list(
+                        beta_hat = "matrix",
+                        y_hat = "matrix",
+                        res = "matrix",
+                        df = "numeric",
+                        res_var = "numeric",
+                        beta_hat_var = "matrix",
+                        t_values = "matrix"
+                      ),
+                      methods = list(
+                        initialize = function(data, formula) {
+                          X <- model.matrix(formula, data = data)
+                          y <- iris[,all.vars(formula)[1]]
+                          beta_hat <<- solve(t(X) %*% X) %*% t(X) %*% y
+                          y_hat <<- X %*% beta_hat
+                          res <<- y - y_hat
+                          df <<- nrow(X) - ncol(X)
+                          res_var <<- as.numeric((t(res) %*% res) / df)
+                          beta_hat_var <<- res_var * (solve(t(X) %*% X))
+                          t_values <<- beta_hat / sqrt(diag(beta_hat_var))
+                        },
+                        print = function() {
+                          cat("test!")
+                        },
+                        plot = function() {
+                          ggplot(data = data.frame(y_hat, res), aes(x = y_hat, y = res)) +
+                            geom_point(shape = 21, colour = "black", fill = "white") +
+                            geom_path(data = as.data.frame(with(data.frame(y_hat, res), lowess(x = y_hat, y = res))),
+                                      aes(x = x, y = y), col = "red") +
+                            ggtitle("Residuals vs Fitted") +
+                            labs(x = "Fitted values", y = "Residuals") +
+                            theme_bw() +
+                            theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                                  plot.title = element_text(hjust = 0.5))
+                        },
+                        resid = function() {
+                          return(c(res))
+                        },
+                        pred = function() {
+                          return(c(y_hat))
+                        },
+                        coef = function() {
+                          coef_vec <- c(beta_hat)
+                          names(coef_vec) <- row.names(beta_hat)
+                          cat(colnames(beta_hat))
+                          return(coef_vec)
+                        },
+                        summary = function() {
+                          cat("summary!")
+                        }
+                      ))
