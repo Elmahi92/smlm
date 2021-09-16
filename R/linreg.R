f9cd15e367dadf2667e01163dc66582188e37282
Mohamed Ali <mohamedelmahi92@outlook.com>
2021-09-16 14:47:16 +0200

    Adding StdRes plot

--- R/linreg.R
+++ R/linreg.R
@@ -6,7 +6,8 @@ linreg <- setRefClass("linreg",
                         df = "numeric",
                         res_var = "numeric",
                         beta_hat_var = "matrix",
-                        t_values = "matrix"
+                        t_values = "matrix",
+                        resstd = "matrix"
                       ),
                       methods = list(
                         initialize = function(data, formula) {
@@ -19,17 +20,29 @@ linreg <- setRefClass("linreg",
                           res_var <<- as.numeric((t(res) %*% res) / df)
                           beta_hat_var <<- res_var * (solve(t(X) %*% X))
                           t_values <<- beta_hat / sqrt(diag(beta_hat_var))
+                          resstd<<- na.omit(sqrt(res/res_var),0)
                         },
                         print = function() {
                           cat("test!")
                         },
-                        plot = function() {
+                        plot_res = function() {
                           ggplot(data = data.frame(y_hat, res), aes(x = y_hat, y = res)) +
                             geom_point(shape = 21, colour = "black", fill = "white") +
                             geom_path(data = as.data.frame(with(data.frame(y_hat, res), lowess(x = y_hat, y = res))),
                                       aes(x = x, y = y), col = "red") +
                             ggtitle("Residuals vs Fitted") +
                             labs(x = "Fitted values", y = "Residuals") +
+                            theme_bw() +
+                            theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                                  plot.title = element_text(hjust = 0.5))
+                        },
+                        plot_resstd = function() {
+                          ggplot(data = data.frame(y_hat, resstd), aes(x = y_hat, y = resstd)) +
+                            geom_point(shape = 21, colour = "black", fill = "white") +
+                            geom_path(data = as.data.frame(with(data.frame(y_hat,resstd ), lowess(x = y_hat, y = resstd))),
+                                      aes(x = x, y = y), col = "red") +
+                            ggtitle("Scale−Location") +
+                            labs(x = "Fitted values", y = "Standardized residuals") +
                             theme_bw() +
                             theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
                                   plot.title = element_text(hjust = 0.5))
