ea16cf66e655029368cd331cf2dd2be096bbba49
Mohamed Ali <mohamedelmahi92@outlook.com>
2021-10-26 18:43:16 +0200

    commit back everything

--- tests/testthat/test_ridgereg.R
+++
@@ -1,9 +0,0 @@
-context("ridgereg_unittest")
-library(MASS)
-data("mtcars")
-
-test_that("Compare coefficients", {
-    regmodel <- reg_model(mpg ~ disp + hp + drat + wt + qsec,data = mtcars)
-    lmridge_mode <- lm.ridge(mpg ~ disp + hp + drat + wt + qsec,data = mtcars)
-    expect_equal(round(regmodel$coef()[-1],1), round(lmridge_mode$coef,1))
-})
